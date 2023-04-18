cca15a66eba0ac7bac39d902645a0ffe5df8845d
OllerBelenAgustina <belenoller8@gmail.com>
2023-04-18 20:04:27 -0300

    Prueba

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,12 @@
+
+
+
+const ItemList = ({products }) =>  {
+    return(
+        <div className='ListGroup'>
+            {products.map(prod  => <Item key={prod.id}{...prod} />) }
+        </div>
+    )
+}
+
+export default ItemList
